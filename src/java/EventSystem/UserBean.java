676664409c58b9b321104945d75cfa538ca9e62f
Poornima197 <69973609+Poornima197@users.noreply.github.com>
2020-09-23 23:01:30 +0530

    userbean

---
+++ src/java/EventSystem/UserBean.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EventSystem;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author HP
+ */
+public class UserBean implements Serializable{
+
+    /**
+     * @return the fname
+     */
+    public String getFname() {
+        return fname;
+    }
+
+    /**
+     * @param fname the fname to set
+     */
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    /**
+     * @return the lname
+     */
+    public String getLname() {
+        return lname;
+    }
+
+    /**
+     * @param lname the lname to set
+     */
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the uname
+     */
+    public String getUname() {
+        return uname;
+    }
+
+    /**
+     * @param uname the uname to set
+     */
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    private String fname;
+    private String lname;
+    private String email;
+    private String uname;
+    private String password;
+    
+}
